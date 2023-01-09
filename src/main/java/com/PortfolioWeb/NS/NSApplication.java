dfdb49b235ddacac0abb17db350a70fcbeab2ea0
Natalia Schwindt <naty8014@gmail.com>
2023-01-09 16:13:36 -0300

    Primer commit

---
+++ src/main/java/com/PortfolioWeb/NS/NSApplication.java
@@ -0,0 +1,60 @@
+package com.PortfolioWeb.NS;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NSApplication.class, args);
+	
+                //User User1 = new User("snaty25", "1234", "naty8014@gmail.com");
+        
+                //System.out.println(User1.Login("snaty25", "1234"));
+        }     
+        
+                       
+        /*Educacion Educacion1 = new Educacion("Full Stack Developer Junior", "Argentina Programa 3.0", 2022, "");
+        Educacion Educacion2 = new Educacion("Curso de Desarrollo con Stack Mean/Mern", "Talleres Don Web - Online", 2022, "");
+        Educacion Educacion3 = new Educacion("Soporte Operativo para PC", "Inst. Sup. de Form. Doc. y Téc. Nº48 - Cnel. Suárez", 2000, "");
+        
+        Educacion[] estudios = {Educacion1, Educacion2, Educacion3};
+        
+        ExperienciaLaboral ExperienciaLaboral1 = new ExperienciaLaboral("Simplia", "Limpieza en Bancos", "Si", 2018, 2022, "Estoy a cargo de la limpieza de un banco", "");
+        ExperienciaLaboral ExperienciaLaboral2 = new ExperienciaLaboral("Prosegur", "Seguridad Privada", "No", 2012, 2016, "Estuve a cargo de un grupo de personas y de la seguridad de una fábrica de zapatillas", "");
+        
+        ExperienciaLaboral[] explab = {ExperienciaLaboral1, ExperienciaLaboral2};
+        
+        Tecnologia Tecnologia1 = new Tecnologia("html5", 80);
+        Tecnologia Tecnologia2 = new Tecnologia("css3", 70);
+        Tecnologia Tecnologia3 = new Tecnologia("sass", 90);
+        Tecnologia Tecnologia4 = new Tecnologia("boostrap", 10);
+        Tecnologia Tecnologia5 = new Tecnologia("javascript", 50);
+        Tecnologia Tecnologia6 = new Tecnologia("typescript", 50);
+        Tecnologia Tecnologia7 = new Tecnologia("angular", 60);
+        Tecnologia Tecnologia8 = new Tecnologia("mysql", 50);
+        Tecnologia Tecnologia9 = new Tecnologia("java", 50);
+        Tecnologia Tecnologia10 = new Tecnologia("springboot", 60);
+        Tecnologia Tecnologia11 = new Tecnologia("trabajoEnEquipo", 80);
+        Tecnologia Tecnologia12 = new Tecnologia("liderazgo", 90);
+        Tecnologia Tecnologia13 = new Tecnologia("comunicacionAsertiva", 90);
+        Tecnologia Tecnologia14 = new Tecnologia("atencionAlCliente", 90);
+        Tecnologia Tecnologia15 = new Tecnologia("capacidadAnalitica", 60);
+        Tecnologia Tecnologia16 = new Tecnologia("atencionAlDetalle", 95);
+        Tecnologia Tecnologia17 = new Tecnologia("deseosDeAprender", 98);
+        Tecnologia Tecnologia18 = new Tecnologia("adaptabilidad", 60);
+        Tecnologia Tecnologia19 = new Tecnologia("scrum", 40);
+        
+        Tecnologia[] tecno = {Tecnologia1, Tecnologia2, Tecnologia3, Tecnologia4, Tecnologia5, Tecnologia6, Tecnologia7, Tecnologia8, Tecnologia9, Tecnologia10, Tecnologia11, Tecnologia12, Tecnologia13, Tecnologia14, Tecnologia15, Tecnologia16, Tecnologia17, Tecnologia18, Tecnologia19};
+        
+        Proyecto Proyecto1 = new Proyecto("DCards", "En proceso", "Esta es una página de tarjetas electrónicas. No sólo contiene los datos del comercio, sino que además se puede acceder a todos sus productos y comprarlos. Se puede gestionar el envío y pagar.");
+        Proyecto Proyecto2 = new Proyecto("onlysoncomic.com", "En proceso", "Esta es una página de comics. Consta de varias páginas sdonde se puede acceder a los capítulos, material extra y ponerse en contacto con la autora.");
+        Proyecto Proyecto3 = new Proyecto("Recetas para todos los días", "En proceso", "Esta es una página de recetas realizadas cotidianamente. Sirve como un recuerdo de recetas sencillas al momento de decidir qué cocinar todos los días. Pueden ver recetas, enviar recetas para que las agregue una vez probadas, o ponerse en contacto conmigo.");
+        
+        Proyecto[] proy = {Proyecto1, Proyecto2, Proyecto3};*/
+
+        
+}       
+        

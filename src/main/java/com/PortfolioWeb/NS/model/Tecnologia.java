084ebccfffac8203ff25fa0365832be103b02254
Natalia Schwindt <naty8014@gmail.com>
2023-02-03 09:12:56 -0300

    Segundo commit

--- src/main/java/com/PortfolioWeb/NS/model/Tecnologia.java
+++ src/main/java/com/PortfolioWeb/NS/model/Tecnologia.java
@@ -1,21 +1,27 @@
 package com.PortfolioWeb.NS.model;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 @Entity
+@Table(name="tecnologia")
 public class Tecnologia implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Long id;
+    @Column(name="nombre")
     private String nombre;
+    @Column(name="porcentajeConocimiento")
     private Integer porcentajeConocimiento;
 
     public Tecnologia() {

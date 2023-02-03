b44f998af3ba5d2a7ade73124525e6b763983ffa
Natalia Schwindt <naty8014@gmail.com>
2023-02-03 09:12:31 -0300

    Segundo commit

--- src/main/java/com/PortfolioWeb/NS/model/Educacion.java
+++ src/main/java/com/PortfolioWeb/NS/model/Educacion.java
@@ -1,23 +1,31 @@
 package com.PortfolioWeb.NS.model;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 @Entity
+@Table(name="educacion")
 public class Educacion implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Long id;
+    @Column(name="estudioCursado")
     private String estudioCursado;
+    @Column(name="nombreInstitucion")
     private String nombreInstitucion; 
+    @Column(name="año")
     private Integer año;
+    @Column(name="urlLogo")
     private String urlLogo;
 
     public Educacion() {

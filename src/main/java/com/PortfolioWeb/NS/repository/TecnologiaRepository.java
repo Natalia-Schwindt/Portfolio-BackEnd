f45316b4e8c9c2bf38ff361757a9c418529a463a
Natalia Schwindt <naty8014@gmail.com>
2023-02-03 09:13:11 -0300

    Segundo commit

--- src/main/java/com/PortfolioWeb/NS/repository/TecnologiaRepository.java
+++ src/main/java/com/PortfolioWeb/NS/repository/TecnologiaRepository.java
@@ -1,10 +1,10 @@
 package com.PortfolioWeb.NS.repository;
 
 import com.PortfolioWeb.NS.model.Tecnologia;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TecnologiaRepository extends JpaRepository <Tecnologia, Long>{
-    
+public interface TecnologiaRepository extends CrudRepository <Tecnologia, Long>{
+
 }

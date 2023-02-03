448d2ad834814e155c44d99664e0c04c08d85ced
Natalia Schwindt <naty8014@gmail.com>
2023-02-03 09:12:36 -0300

    Segundo commit

--- src/main/java/com/PortfolioWeb/NS/model/ExpLab.java
+++ src/main/java/com/PortfolioWeb/NS/model/ExpLab.java
@@ -1,26 +1,37 @@
 package com.PortfolioWeb.NS.model;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 @Entity
+@Table(name="experiencialaboral")
 public class ExpLab implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Long id;
+    @Column(name="nombreEmpresa")
     private String nombreEmpresa;
+    @Column(name="puesto")
     private String puesto;  
-    private String esTrabajoActual;      
+    @Column(name="esTrabajoActual")
+    private String esTrabajoActual;    
+    @Column(name="fechaInicio")
     private Integer fechaInicio;
-    private Integer fechaFin;      
+    @Column(name="fechaFin")
+    private Integer fechaFin;  
+    @Column(name="descripcion")
     private String descripcion;
+    @Column(name="urlLogo")
     private String urlLogo;
 
     public ExpLab() {
